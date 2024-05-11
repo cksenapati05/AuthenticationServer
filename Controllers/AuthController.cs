902eaf72a30c53dcf8dd4a79862f5c1f3a31685a
hp <hp@LAPTOP-O0J1GUAC>
2024-05-11 09:01:45 +0530

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationServer.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost(Name ="GetJwtToken")]
+        public IDictionary<string, string> GetJwtToken([FromBody] RequestPayload payload)
+        {
+            if (payload.Id == "cksenapati05@gmail.com" && payload.Password == "validPassword")
+            {
+                var token = GenerateJwtToken();
+                return new Dictionary<string, string>
+                {
+                    { "token", token }
+                };
+            }
+            else
+                throw new UnauthorizedAccessException();
+        }
+
+        [HttpGet]
+        public string GetIssuerName()
+        {
+            return "cksenapati";
+        }
+
+        private string GenerateJwtToken()
+        {
+            var algorithm = SecurityAlgorithms.HmacSha256;
+
+            var claimsCollection = new List<Claim> {
+                new Claim("Name", "Chandan"),
+                new Claim("id", "1001")
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("ThisIsMySecurityKeyThatisOf256bits"));
+
+            var signingCredential = new SigningCredentials(securityKey, algorithm);
+
+            var token = new JwtSecurityToken(
+                    issuer: "cksenapati",
+                    audience: "cksenapati",
+                    claimsCollection,
+                    expires: DateTime.Now.AddMinutes(2),
+                    signingCredentials: signingCredential
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
+    public class RequestPayload
+    {
+        public string? Id { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AuthenticationServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = "cksenapati",
+                        ValidateAudience = true,
+                        ValidAudience = "cksenapati",
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("ThisIsMySecurityKeyThatisOf256bits"))
+                    };
+                });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
